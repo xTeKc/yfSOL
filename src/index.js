8dac7353d621bd495dd97b8ece2b3e877fcfed85
xTeKc <bcd3v@pm.me>
2021-07-24 12:47:50 -0400

    add new imports

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ReactGA from "react-ga";
+import { isMobile } from "react-device-detect";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
