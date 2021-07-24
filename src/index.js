b670c72553ee1886b3066c02e96a42d4263c1d00
xTeKc <bcd3v@pm.me>
2021-07-24 12:48:43 -0400

    init ga

--- src/index.js
+++ src/index.js
@@ -7,6 +7,21 @@ import reportWebVitals from './reportWebVitals';
 import ReactGA from "react-ga";
 import { isMobile } from "react-device-detect";
 
+// initialize GA
+const GOOGLE_ANALYTICS_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+if (typeof GOOGLE_ANALYTICS_ID === "string") {
+  ReactGA.initialize(GOOGLE_ANALYTICS_ID);
+  ReactGA.set({
+    customBrowserType: !isMobile
+      ? "desktop"
+      : "web3" in window || "ethereum" in window
+      ? "mobileWeb3"
+      : "mobileRegular",
+  });
+} else {
+  ReactGA.initialize("test", { testMode: true, debug: true });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
